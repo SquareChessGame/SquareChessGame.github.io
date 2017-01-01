73a0126ad91a57ebc0b246911374a531330ea4be
SquareChessGame <SquareChessGame@gmail.com>
2017-01-01 12:32:59 +0800

    Add files via upload

--- Mode/Scheme.js
+++ Mode/Scheme.js
@@ -4,7 +4,7 @@ Shl.Rul.Scheme=function(){
 	if(Tn>2)for(var i=0;i<2;i++){
 		if(Ara(Shl.Ara.Scheme[i],Sqr.Sym[i]+"A")||Ara(Shl.Ara.Scheme[i],Sqr.Sym[(i+1)%2]+">0"))return Sqr.Sym[(i+1)%2]+"獲勝"
 	}Shl.Ara.Scheme=[[],[]]
-	for(var i=0;i<2;i++)if(Tn>i)Shl.Ara.Scheme[i]=Flt(Crd(Hst.Crd[i+1],"8"),function(crd){if(Qre(crd,"Sym")!=3&&Shl.Ara.Scheme[0].indexOf(crd)<0)return 1;return 0})
+	for(var i=0;i<2;i++)if(Tn>i)Shl.Ara.Scheme[i]=Flt(Crd(Hst.Crd[i+1],"8"),function(crd){if(Qre(crd,"Sym")!=3&&Shl.Ara.Scheme[0].indexOf(crd)<0&&crd!=Hst.Crd[1]&&crd!=Hst.Crd[2])return 1;return 0})
 	if(Tn==1)Shl.Ara.Scheme.Lmt=Flt(Crd(Hst.Crd[1],["28","Q","8"]),function(crd){if(Qre(crd,"Sym")==2)return 1;return 0})
 }
 Shl.Lmt.Scheme=function(crd,sym){
