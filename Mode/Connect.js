0a4c2e0f0286685d765bbe68fa3847a641575597
SquareChessGame <SquareChessGame@gmail.com>
2017-01-20 12:54:40 +0800

    Add files via upload

--- Mode/Connect.js
+++ Mode/Connect.js
@@ -41,22 +41,22 @@ Shl.Ato.Connect=function(typ){
 		}
 	}
 	if(!typ){if(mn[Sqr.Sym[Tn%2]].length<mn[Sqr.Sym[(Tn+1)%2]].length)typ="A";else typ="D"}
-	while(ext.length!=crd.length){var row=0,ckr=0
+	while(ext.length!=crd.length){var r=0,k=0
 		for(var i=0;i<nx.length;i++){if(ext.indexOf(i)>-1)continue
-			if(!ckr){row=i;ckr=1;continue}
+			if(!k){r=i;k=1;continue}
 			var tk0c=nx[i][Sqr.Sym[Tn%2]].All.length-nx[i][Sqr.Sym[(Tn+1)%2]].All.length,
-				tk0m=nx[row][Sqr.Sym[Tn%2]].All.length-nx[row][Sqr.Sym[(Tn+1)%2]].All.length,
-				tk1c=ctl[Sqr.Sym[Tn%2]][i],tk1m=ctl[Sqr.Sym[Tn%2]][row],
-				tk2c=ctl[Sqr.Sym[(Tn+1)%2]][i],tk2m=ctl[Sqr.Sym[(Tn+1)%2]][row]
-			if(tk0c>tk0m)row=i
+				tk0m=nx[r][Sqr.Sym[Tn%2]].All.length-nx[r][Sqr.Sym[(Tn+1)%2]].All.length,
+				tk1c=ctl[Sqr.Sym[Tn%2]][i],tk1m=ctl[Sqr.Sym[Tn%2]][r],
+				tk2c=ctl[Sqr.Sym[(Tn+1)%2]][i],tk2m=ctl[Sqr.Sym[(Tn+1)%2]][r]
+			if(tk0c>tk0m)r=i
 			else if(tk0c==tk0m){
 				if(typ=="A"){
-					if(tk1c>tk1m)row=i;else if(tk1c==tk1m&&(tk2c>tk2m||tk2c==tk2m))row=i
+					if(tk1c>tk1m)r=i;else if(tk1c==tk1m&&(tk2c>tk2m||tk2c==tk2m))r=i
 				}else{
-					if(tk2c>tk2m)row=i;else if(tk2c==tk2m&&(tk1c>tk1m||tk1c==tk1m))row=i
+					if(tk2c>tk2m)r=i;else if(tk2c==tk2m&&(tk1c>tk1m||tk1c==tk1m))r=i
 				}
 			}
-		}ext.push(row)
+		}ext.push(r)
 	}if(typ=="A")ext.reverse()
 	return {mn,nx,crd,ext,ctl,typ,set:crd[ext[0]]}
 }
