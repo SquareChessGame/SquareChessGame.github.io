f04811472c279cd7d20090a755aec0b2e5da9155
SquareChessGame <SquareChessGame@gmail.com>
2017-01-23 12:59:18 +0800

    Add files via upload

--- Core/Mgbx.js
+++ Core/Mgbx.js
@@ -1,10 +1,4 @@
 function Mbx(m,a,b,v,s){$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc").childNodes[0].innerHTML=m;console.log(v)
-	$("#Mbxsl").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)-1
-		if(p<0)p=Mbx.stq.length-1;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
-	})
-	$("#Mbxsr").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)+1
-		if(p>Mbx.stq.length-1)p=0;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
-	})
 	Id("Mbxt").addEventListener("change",function(){Mbx.inp=this.value})
 	$("#MbxbY").click(function(){Id("Mbxt").blur();a(Mbx.inp);Id("Mbx").style.display="none"})
 	if(b){Id("MbxbN").style.display="";$("#MbxbN").click(function(){b();Id("Mbx").style.display="none"})}
@@ -15,3 +9,9 @@ function Mbx(m,a,b,v,s){$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc")
 }
 Mbx.Rsz=function(){$("#Mbx").css("top",($(window).height()/2-100)+"px").css("left",($(window).width()/2-200)+"px")}
 Mbx.stq=[];Mbx.inp="";Mbx.Rsz()
+$("#Mbxsl").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)-1
+		if(p<0)p=Mbx.stq.length-1;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
+})
+$("#Mbxsr").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)+1
+	if(p>Mbx.stq.length-1)p=0;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
+})
