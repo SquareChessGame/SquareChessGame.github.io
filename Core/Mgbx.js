e7ce6ea07e2d03b3e69a7f10bb4893c192bc0982
SquareChessGame <SquareChessGame@gmail.com>
2017-01-23 21:05:20 +0800

    Add files via upload

--- Core/Mgbx.js
+++ Core/Mgbx.js
@@ -1,16 +1,20 @@
 function Mbx(m,a,b,v,s){$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc").childNodes[0].innerHTML=m
 	Id("Mbxt").addEventListener("change",function(){Mbx.inp=this.value})
-	$("#MbxbY").click(function(){Id("Mbxt").blur();a(Mbx.inp);Id("Mbx").style.display="none"})
-	if(b){Id("MbxbN").style.display="";$("#MbxbN").click(function(){b();Id("Mbx").style.display="none"})}
+	$("#MbxbY").unbind('click').click(function(){Mbx.Exe(a)})
+	if(b){Id("MbxbN").style.display="";$("#MbxbN").unbind('click').click(function(){Mbx.Exe(b)})}
 	switch(typeof v){
 		case"string":Id("Mbxt").style.display="";Id("Mbxt").value=v;break
 		case"object":Mbx.stq=v;Id("Mbxp").style.display="";if(!s)s=0;Id("Mbxs").value=v[s];break
 	}Id("Mbx").style.display=""
 }
 Mbx.Rsz=function(){$("#Mbx").css("top",($(window).height()/2-100)+"px").css("left",($(window).width()/2-200)+"px")}
-Mbx.stq=[];Mbx.inp="";Mbx.Rsz()
+Mbx.Ldr=function(){
+	Id("Mbx").innerHTML="<div id=\"Mbxc\"><div style=\"margin:10px;color:#48585f\"></div></div><div style=\"position:absolute;bottom:5px;left:10px\"><input type=\"text\" id=\"Mbxt\" style=\"display:none\"><div style=\"display:none\" id=\"Mbxp\"><div class=\"Mbxs\" style=\"float:left\" id=\"Mbxsl\"><</div><input type=\"text\" id=\"Mbxs\"/><div class=\"Mbxs\" style=\"float:right\" id=\"Mbxsr\">></div></div></div><div style=\"position:absolute;bottom:5px;right:10px;height:40px;width:125px\"><div class=\"Mbxb\" id=\"MbxbN\" style=\"margin-left:5px;display:none\">取消</div><div class=\"Mbxb\" id=\"MbxbY\">確認</div></div>";Mbx.Rsz()
+}
+Mbx.stq=[];Mbx.inp="";Mbx.Ldr()
+Mbx.Exe=function(a){a(Mbx.inp);Id("Mbx").style.display="none"}
 $("#Mbxsl").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)-1
-		if(p<0)p=Mbx.stq.length-1;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
+	if(p<0)p=Mbx.stq.length-1;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
 })
 $("#Mbxsr").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)+1
 	if(p>Mbx.stq.length-1)p=0;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
