df1f94c558bd971478cef9f11362e5f0561f0da9
SquareChessGame <SquareChessGame@gmail.com>
2017-01-24 11:24:55 +0800

    Add files via upload

--- Core/Mgbx.js
+++ Core/Mgbx.js
@@ -1,4 +1,4 @@
-function Mbx(m,a,b,v,s){$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc").childNodes[0].innerHTML=m
+function Mbx(m,a,b,v,s){$("#Mbx,#Mbxv").css("display","");$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc").childNodes[0].innerHTML=m
 	Id("Mbxt").addEventListener("change",function(){Mbx.inp=this.value})
 	$("#MbxbY").unbind('click').click(function(){
 		switch(typeof v){
@@ -12,7 +12,7 @@ function Mbx(m,a,b,v,s){$("#Mbxt,#Mbxp,#MbxbN").css("display","none");Id("Mbxc")
 		case"object":Mbx.stq=v;Id("Mbxp").style.display="";if(!s)s=0;Id("Mbxs").value=v[s];break
 	}Id("Mbx").style.display=""
 }
-Mbx.Rsz=function(){$("#Mbxt,#Mbxp,#Mbxs").css("width","");$("#Mbx").css("width","400px")
+Mbx.Rsz=function(){$("#Mbxt,#Mbxp,#Mbxs").css("width","");$("#Mbx").css("width","400px");$("#Mbxv").css("width",$(window).width()+"px");$("#Mbxv").css("height",$(window).height()+"px")
 	if($(window).width()<400){$("#Mbx").css("width","100%")
 		var w=$(window).width()-132-$(window).width()*0.05
 		$("#Mbxt,#Mbxp").css("width",w+"px");$("#Mbxs").css("width",w-70+"px")
@@ -20,9 +20,10 @@ Mbx.Rsz=function(){$("#Mbxt,#Mbxp,#Mbxs").css("width","");$("#Mbx").css("width",
 }
 Mbx.Ldr=function(){
 	Id("Mbx").innerHTML="<div id=\"Mbxc\"><div style=\"margin:10px;color:#48585f\"></div></div><div style=\"position:absolute;bottom:5px;left:10px\"><input type=\"text\" id=\"Mbxt\" style=\"display:none\"><div style=\"display:none\" id=\"Mbxp\"><div class=\"Mbxs\" style=\"float:left\" id=\"Mbxsl\"><</div><input type=\"text\" id=\"Mbxs\"/><div class=\"Mbxs\" style=\"float:right\" id=\"Mbxsr\">></div></div></div><div style=\"position:absolute;bottom:5px;right:10px;height:40px;width:125px\"><div class=\"Mbxb\" id=\"MbxbN\" style=\"margin-left:5px;display:none\">取消</div><div class=\"Mbxb\" id=\"MbxbY\">確認</div></div>";Mbx.Rsz()
+	$("#Mbx,#Mbxv").css("display","none")
 }
 Mbx.stq=[];Mbx.inp="";Mbx.Ldr()
-Mbx.Exe=function(a){a(Mbx.inp);Id("Mbx").style.display="none"}
+Mbx.Exe=function(a){a(Mbx.inp);$("#Mbx,#Mbxv").css("display","none")}
 $("#Mbxsl").click(function(){var p=Mbx.stq.indexOf(Id("Mbxs").value)-1
 	if(p<0)p=Mbx.stq.length-1;Id("Mbxs").value=Mbx.stq[p];Mbx.inp=Mbx.stq[p]
 })
