596301f47fb9ed6cb7ba0de7f9dfb4d2357b8eb4
SquareChessGame <SquareChessGame@gmail.com>
2017-02-07 21:47:19 +0800

    Add files via upload

--- Mode/Fallen.js
+++ Mode/Fallen.js
@@ -1,6 +1,5 @@
 Shl.Ara.Fallen=[];Dft.Fallen={Fln:5}
-Shl.Rul.Fallen=function(){
-}
+Shl.Rul.Fallen=function(){}
 Shl.Lmt.Fallen=function(){}
 Shl.Mrk.Fallen=function(){
 	Qre(Flt(Sel("All"),function(crd){if(Qre(crd,"Sym")==4)return 1;return 0}),["BgC","FtC"],[12,2])
@@ -13,6 +12,7 @@ Shl.Ckr.Fallen=function(crd,set){var mcd=crd
 	}).length>Dft.Fallen.Fln)Mbx("去除對方符號",function(crd){
 		Qre([crd,mcd],["Sym","BgC","FtC"],[4,12,2]);Tn++;
 		Hst.Crd[Tn]=mcd;Rul();Hst.Brd[Tn]=Rec();Sel.Now("N");Log()
+		if(Dft.System.Oln)Upl(Hst.Brd[Tn]+"/"+Tn+"/"+Hst.Crd[Tn])
 	},function(){},Flt(Sel("All"),function(crd){
 		if(Qre(crd,"Sym")==(Tn+1)%2)return 1;return 0
 	}));return 1
@@ -23,5 +23,5 @@ Shl.Opt.Fallen=function(){
 }
 Shl.OpK.Fallen=function(){
 	if(!Dft.System.Oln&&Val(Id("Fallen-Fln").value)!=NaN&&Id("Fallen-Fln").value!="")Dft.Fallen.Fln=Val(Id("Fallen-Fln").value)
-	if(Dft.Fallen.Fln<10)Dft.Fallen.Fln=10
+	if(Dft.Fallen.Fln<5)Dft.Fallen.Fln=5
 }
