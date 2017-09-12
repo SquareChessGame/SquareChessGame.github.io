bda76c570b383fad1aee521c6e54d6b45dd69ad9
SquareChessGame <SquareChessGame@gmail.com>
2017-09-12 22:26:31 +0800

    Update Forbid.js

--- Mode/Forbid.js
+++ Mode/Forbid.js
@@ -1,9 +1,11 @@
 Shl.Ara.Forbid=[];Dft.Forbid={}
 Shl.Ckr.Forbid=function(crd){var sym=Tn%2
 	for(var i=MdQ.length-1;i>-1;i--){if(MdQ[i]=="Forbid")continue
-		if(Shl.Ara[MdQ[i]][Sqr.Sym[sym]].All.indexOf(crd)>-1||
-		Shl.Ara[MdQ[i]][Sqr.Sym[(sym+1)%2]].All.indexOf(crd)>-1)return 0
-		if(Shl.Lmt[MdQ[i]](crd,sym)||Shl.Lmt[MdQ[i]](crd,(sym+1)%2))return 0
+		if(Shl.Ara[MdQ[i]])if(
+			Shl.Ara[MdQ[i]][Sqr.Sym[sym]].All.indexOf(crd)>-1||
+			Shl.Ara[MdQ[i]][Sqr.Sym[(sym+1)%2]].All.indexOf(crd)>-1
+		)return 0
+		if(Shl.Lmt[MdQ[i]])if(Shl.Lmt[MdQ[i]](crd,sym)||Shl.Lmt[MdQ[i]](crd,(sym+1)%2))return 0
 	}return 1
 }
 Shl.OpK.Forbid=function(){Dft.System.Qsr=0}
